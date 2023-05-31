345ca6ea33f731cd62c9fe535aca0782a5ec7350
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-05-31 18:07:50 +0200

    Add JS from DOM manipulation workshop

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello HTML!");
+
+document.body.innerHTML = "";
+
+document.body.insertAdjacentHTML("beforeend", '<button id="add-paragraph">Add new paragraph</button>');
+document.body.insertAdjacentHTML("beforeend", '<strong id="counter">HELLO</strong>')
+
+const button = document.getElementById("add-paragraph");
+const counterElement = document.getElementById("counter");
+
+let counter = 0;
+
+button.addEventListener("click", function () {
+    counter += 1;
+    counterElement.textContent = counter;
+    document.body.insertAdjacentHTML("beforeend", "<p>My new paragraph</p>");
+});
